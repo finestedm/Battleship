690176123f2d24b1e4644d16a3125365628e772f
Paweł <finestedm@gmail.com>
2022-10-09 18:49:41 +0200

    name input added

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,5 +1,7 @@
 import { Player, players } from "./factories/player";
 import { createGameboardDOM, regenerateGameboard } from "./DOM";
+import { showModal } from "./showModal";
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 
 const playerOne = new Player('Paweł', false)
 const playerTwo = new Player('pieseł', true)
@@ -10,10 +12,19 @@ export function changePlayer() {
     activePlayer === playerOne && letPCAttack()
 }
 
+showModal();
+var myModal = new bootstrap.Modal(document.getElementById("my-modal"), {});
+myModal.show()
+const nameButton = document.getElementById('btn-name-save')
+nameButton.addEventListener('click', () => {
+    const newPlayerName = document.getElementById('new-player-name').value;
+    playerOne.name = newPlayerName
+    myModal.hide()
+})
 createGameboardDOM(playerOne)
 
-
 export function gameLoop() {
+
 
     while (playerTwo.gameboard.unusedShips.length > 0) {
         playerTwo.gameboard.placeShip(playerTwo.gameboard.unusedShips[playerTwo.gameboard.unusedShips.length - 1], generateRandomStartingLocation(), generateRandomDirection())
---
+++ src/modules/showModal.js
@@ -0,0 +1,26 @@
+import { players } from "./factories/player";
+
+export function showModal() {
+    const modal = document.createElement('modal');
+    const body = document.querySelector('body')
+    modal.innerHTML =
+        `<div class="modal"  data-bs-backdrop="static" data-bs-keyboard="false" id="my-modal"  tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Choose player name</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div class="input-group">
+                            <span class="input-group-text">Player Name</span>
+                            <input type='text' id='new-player-name' class="form-control" aria-label="Player name"></input>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" id='btn-name-save' class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    body.append(modal)
+}
